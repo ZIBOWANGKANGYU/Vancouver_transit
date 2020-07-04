deddece814b7a59385bd25b7507f4775d76ee941
Kangyu Wang <kaw129@ucsd.edu>
2020-07-04 16:05:30 -0400

    Downloaded census data and made census related maps.

--- Census_reading.py
+++ Census_reading.py
@@ -9,6 +9,8 @@ import os
 cwd = os.path.dirname(os.getcwd())
 os.chdir(cwd)
 data_dir = os.path.join(os.getcwd(), "Census")
+
+data_version = "20200606"
 # Federal Electoral Districts
 file = geopandas.read_file(os.path.join(data_dir, "lfed000b16a_e","lfed000b16a_e.shp"))
 BC_FED = file.loc[file["PRNAME"]=='British Columbia / Colombie-Britannique']
@@ -24,4 +26,67 @@ file = geopandas.read_file(os.path.join(data_dir, "lda_000b16a_e","lda_000b16a_e
 BC_DA = file.loc[file["PRNAME"]=='British Columbia / Colombie-Britannique']
 BC_DA.to_file(os.path.join(os.getcwd(), "Census", "BC_DA.shp"))
 
-BC_DA.loc[BC_DA["CDNAME"] == "Greater Vancouver"].plot()##Greater Vancouver Area
+GVA_DA = BC_DA.loc[BC_DA["CDNAME"] == "Greater Vancouver"]##Greater Vancouver Area
+GVA_DA.DAUID.max()
+GVA_DA.DAUID.min()
+
+#Read DA level data
+from dbfread import DBF
+import pandas as pd
+import numpy as np
+
+batch_list = ["pdad", "fhm", "hous", "lang", "incm", "immi", "ethn", "labr", "mobl"]# Census data for Greater Vancouver Area is downloaded in batches. 
+
+def read_census_tables(batch_list):
+    lengths = [] 
+    data_table_list = []
+    header_list = []
+    for batch in batch_list:
+        pd_dir = os.path.join(data_dir, "data_2016", batch + ".dbf")
+        table = DBF(pd_dir, load=True)
+        table_df = pd.DataFrame(np.array([list(record.values()) for record in table.records]))
+        lengths.append(len(table.records))
+        header_dir = os.path.join(data_dir, "data_2016", batch + ".txt")
+        header = pd.read_table(header_dir)
+        result = all(element == lengths[0] for element in lengths)
+        if not result:
+            raise ValueError('The input tables are not of the same lengths')
+        data_table_list.append(table_df)
+        header_list.append(header)
+    else: 
+        data_table = pd.concat(data_table_list, axis = 1)
+        data_header = pd.concat(header_list, axis = 0)
+        return data_table, data_header
+
+data_table, header = read_census_tables(batch_list)
+header = [item[0].partition(' - ')[2] for item in header.values]
+data_table.columns = header
+data_table = data_table.loc[:,~data_table.columns.duplicated()]#Remove duplicated columns
+header = data_table.columns
+data_table.columns = ["vn" + str(col) for col in range(len(data_table.columns))]
+
+GVA_DA_header = GVA_DA.columns
+GVA_DA = GVA_DA.merge(data_table, left_on = "ADAUID", right_on = "vn0")
+
+header = list(GVA_DA_header) + list(header)
+
+##Converting data types in GVA_DA
+for column_name in GVA_DA.columns[29:]:
+    GVA_DA[[column_name]] = pd.DataFrame((pd.to_numeric([item[0] for item in GVA_DA[[column_name]].to_numpy()], errors = "coerce")))
+    GVA_DA[[column_name]] = GVA_DA[[column_name]].astype("float64")
+
+##Mapping DAs
+import contextily as ctx
+import matplotlib.pyplot as plt
+GVA_DA = GVA_DA.to_crs(epsg=3857)
+GVA_DA.plot()
+GVA_DA_ax = GVA_DA.plot(figsize=(20, 20), alpha=0.5, column="vn13", cmap = "OrRd", legend = True)
+ctx.add_basemap(GVA_DA_ax, zoom=12)
+plt.title("2016 Population by Dissemination Area")
+plt.savefig(os.path.join(os.getcwd(), "Vancouver_transit", "Maps", data_version,'pop2016.png'))
+
+##Save census tables
+import json
+with open(os.path.join(os.getcwd(), "Data_Tables", data_version,'GVA_DA_data.json'), "w+") as DA_data_outfile:
+    json.dump(GVA_DA.to_json(), DA_data_outfile)
+    
--- Temperal_analysis.py
+++ Temperal_analysis.py
@@ -40,3 +40,57 @@ with open(os.path.join(os.getcwd(), "Data_Tables", data_version,'trips.json'), "
     trips = pd.read_json(json.load(trips_outfile))
 
 #Temporal distribution of all services
+import matplotlib.pyplot as plt
+
+#By arrival time
+def hr_converter(hr_in):
+    if int(hr_in[0:2]) >= 24:
+        hr_out = int(hr_in[0:2]) - 24
+        return str(hr_out).zfill(2) + hr_in[2:]
+    else: 
+        return hr_in
+    
+stop_times["arrival_time_24hr"] = [hr_converter(arrival_time) for arrival_time in stop_times["arrival_time"]]
+
+plt.hist(pd.to_datetime(stop_times['arrival_time_24hr'], format='%H:%M:%S').dt.hour, bins=24)
+plt.ylabel('Count')
+plt.xlabel('Arrival Time')
+plt.title("Number of Services by Arrival Time")
+plt.savefig(os.path.join(os.getcwd(), "Vancouver_transit", "Figures", data_version,'arrivaltime_hist.png'))
+
+#Stops by avearge arrival time 3 am and 4 am
+def hr_converter_cut(hr_in):
+    if int(hr_in[0:2]) <= 3:
+        hr_out = int(hr_in[0:2]) + 24
+        return str(hr_out).zfill(2) + hr_in[2:]
+    else: 
+        return hr_in
+    
+stop_times["arrival_time_day"] = [hr_converter_cut(arrival_time_24hr) for arrival_time_24hr in stop_times["arrival_time_24hr"]]
+stop_times["arrival_time_hour"] = [int (arrival_time_day[0:2]) for arrival_time_day in stop_times["arrival_time_day"]]
+
+import numpy as np
+import contextily as ctx
+stop_times_avrg_arr_t = stop_times.groupby(["stop_id"]).agg(avrg_arrival_time=pd.NamedAgg(column = 'arrival_time_hour', aggfunc = np.mean)).sort_values(by=['avrg_arrival_time'])
+
+plt.hist(stop_times_avrg_arr_t["avrg_arrival_time"], bins = 24)#Except for few stops with very early or late average arrival times, most stops have average arrival times between 10 to 18
+
+stops_gdf = stops_gdf.merge(stop_times_avrg_arr_t, left_on = "stop_id", right_on = "stop_id")
+stops_avrg_arr_t = stops_gdf.loc[(stops_gdf["avrg_arrival_time"]>=10) & (stops_gdf["avrg_arrival_time"]<=18)].plot(figsize=(20, 20), alpha=0.5, column='avrg_arrival_time', cmap = "seismic", legend=True)
+ctx.add_basemap(stops_avrg_arr_t, zoom=12)
+plt.savefig(os.path.join(os.getcwd(), "Vancouver_transit", "Maps", data_version,'stops_avrg_arr_t.png'))#Stops in Vancouver, Burnabay and west Richmond have later average arrival times than stops elsewhere
+
+## Identify stops by whether busy or not
+stops_cnt_trips = stop_times.groupby("stop_id").agg(num_trips = pd.NamedAgg(column = "trip_id", aggfunc = lambda x: len(x.unique()))).sort_values(by = ['num_trips'])
+
+###Make histograms for the stops with most number of trips
+    
+fig, axs = plt.subplots(2,5, figsize=(20, 6), sharex = True, sharey=True)
+fig.subplots_adjust(hspace = .5, wspace=.001)
+axs = axs.ravel()
+for i in range(10):
+    axs[i].hist(pd.to_datetime(stop_times.loc[stop_times["stop_id"] == stops_cnt_trips[-10:].index.tolist()[i]]['arrival_time_24hr'], format='%H:%M:%S').dt.hour, bins = 24)
+    axs[i].set_title(stops_gdf.loc[stops_gdf['stop_id'] == stops_cnt_trips[-10:].index.tolist()[i]]['stop_name'].tolist()[0], fontsize = 11, wrap=True)
+fig.suptitle("Number of Services by Arrival Time: the busiest stops", fontsize=16)
+plt.savefig(os.path.join(os.getcwd(), "Vancouver_transit", "Maps", data_version,'stops_bz_arr_t.png'))#Stops in Vancouver, Burnabay and west Richmond have later average arrival times than stops elsewhere
+
